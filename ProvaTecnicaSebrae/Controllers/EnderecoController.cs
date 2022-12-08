a941d54bbf63cf43baccf872d7188612140f2176
Leandro <Leandro Santos>
2022-12-08 15:37:18 -0300

    implementacao do consumo de endereco por cep

---
+++ ProvaTecnicaSebrae/Controllers/EnderecoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProvaTecnicaSebrae.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProvaTecnicaSebrae.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class EnderecoController : ControllerBase
+    {
+        private readonly IEnderecoService _enderecoService;
+
+        public EnderecoController(IEnderecoService enderecoService)
+        {
+            _enderecoService = enderecoService;
+        }
+
+        [HttpGet]
+        [Route("consulta")]
+        public async Task<IActionResult> Consulta(string cep)
+        {
+            var retorno = await _enderecoService.RetornaEndereco(cep);
+
+            return Ok(retorno);
+        }
+
+    }
+}
---
+++ ProvaTecnicaSebrae/Interfaces/IEnderecoService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProvaTecnicaSebrae.Interfaces
+{
+    public interface IEnderecoService
+    {
+        Task<string> RetornaEndereco(string cep);
+
+    }
+}
